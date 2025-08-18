506f1d0215a2fe0336873a9ebfef50173a68101a
prathamkumawathost <propratham4@gmail.com>
2025-08-18 22:59:39 +0530

    Bug Fix: Login Controller, Explicitly Returning Password from User Record to Compare it with Input Password.

--- controller/authController.js
+++ controller/authController.js
@@ -37,7 +37,7 @@ export async function login(req, res) {
     try {
   const { email, password } = req.body;
 
-    const user = await User.findOne({ email });
+    const user = await User.findOne({ email }).select("+password")
     if (!user) return res.status(404).json({ error: "Email Not Registered" });
 //Get Salt, use it to Compare the Hashed Password with Input Password
     const isPassword = await bcrypt.compare(password, user.password);
